901dddba66b2249fbbbb5a6e88c82d02a0eff2da
BMuscled <bilalmalik42011@gmail.com>
2023-02-05 01:35:16 +0500

    Posts query added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+
+let POSTS = [
+  { id: 1, title: "Post 1" },
+  { id: 2, title: "Post 2" },
+];
+
+let wait = (time) => new Promise((res) => setTimeout(res, time));
+
 function App() {
+  const postsQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () =>
+      wait(1000)
+        .then(() => [...POSTS])
+        .then(() => Promise.reject("No Posts")),
+  });
+
+  if (postsQuery.isLoading) return <p>Loading....</p>;
+  if (postsQuery.error) return <p>{postsQuery.error}</p>;
   return (
     <div>
-      <h1>React Query 2</h1>
+      {POSTS.map((post) => (
+        <h1 key={post.id}>{post.title}</h1>
+      ))}
     </div>
   );
 }
